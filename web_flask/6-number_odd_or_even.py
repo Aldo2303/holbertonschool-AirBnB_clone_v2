53218ae2587fe05e83f98278926239525e9c4316
Aldo2303 <aldod03@gmail.com>
2023-08-16 07:47:46 -0700

    task 6

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -62,8 +62,8 @@ def number_template_n(n):
         return render_template('5-number.html', n=n)
 
 
-@app.route("/number_odd_or_even/<n>", strict_slashes=False)
-def number_template_n(n):
+@app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
+def number_odd_or_even_n(n):
     """Displays a specific message when route is /number_odd_or_even/<n>"""
     if type(n) == int:
         return render_template('6-number_odd_or_even.html', n=n)
